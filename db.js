187537e2ebbdb54c24c02173d4bd6f11bb3efa81
binaryHelicopter <g.sinyakou@knomary.com>
2021-05-22 01:28:05 +0300

    refactor(project): add refactoring

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
-const Sequelize = require('sequelize');
-                                //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: 5433,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-)
+module.exports = sequelize;
